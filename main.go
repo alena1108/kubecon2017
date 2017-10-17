5cef5e7af3502006f7502f35d5029f1280c91b67
Alena Prokharchyk <alena@rancher.com>
2017-10-17 15:49:26 -0700

    auth

--- main.go
+++ main.go
@@ -3,21 +3,59 @@ package main
 import (
 	"os"
 
+	"time"
+
 	"github.com/Sirupsen/logrus"
 	"github.com/urfave/cli"
+	"k8s.io/apimachinery/pkg/apis/meta/v1"
+	"k8s.io/client-go/kubernetes"
+	"k8s.io/client-go/rest"
+	"k8s.io/client-go/tools/clientcmd"
 )
 
 var VERSION = "v0.0.0-dev"
 
 func main() {
 	app := cli.NewApp()
-	app.Name = "kubecon"
-	app.Version = VERSION
-	app.Usage = "You need help!"
-	app.Action = func(c *cli.Context) error {
-		logrus.Info("I'm a turkey")
-		return nil
+	app.Flags = []cli.Flag{
+		cli.StringFlag{
+			Name:  "config",
+			Usage: "Kube config for outside of cluster access",
+		},
 	}
 
+	app.Action = func(c *cli.Context) error {
+		clientset, err := getClient(c.String("config"))
+		if err != nil {
+			return err
+		}
+		for {
+			nodes, err := clientset.Core().Nodes().List(v1.ListOptions{})
+			if err != nil {
+				return err
+			}
+			for _, node := range nodes.Items {
+				logrus.Infof("Node is %v", node.Name)
+			}
+			time.Sleep(5 * time.Second)
+		}
+	}
 	app.Run(os.Args)
+}
+
+func getClient(cfg string) (*kubernetes.Clientset, error) {
+	var config *rest.Config
+	var err error
+	if cfg == "" {
+		logrus.Info("Using in cluster config")
+		config, err = rest.InClusterConfig()
+		// in cluster access
+	} else {
+		logrus.Info("Using out of cluster config")
+		config, err = clientcmd.BuildConfigFromFlags("", cfg)
+	}
+	if err != nil {
+		return nil, err
+	}
+	return kubernetes.NewForConfig(config)
 }
